c2b344bc84e0e4baab9a84975b7a25e41740c3d2
ilyes200876 <ilyes200876@live.fr>
2023-11-05 20:49:33 +0100

    profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/services/user/user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
+
+  userData: any;
+
+  constructor(private userService: UserService){}
+
+  ngOnInit(): void {
+    this.getUserData();
+  }
+
+  getUserData() {
+    this.userService.getUserData().subscribe(
+      (userData) => {
+        console.log('Données de l\'utilisateur connecté :', userData);
+        this.userData = userData;
+      },
+      (error) => {
+        console.error('Erreur :', error);
+      }
+    );
+  }
 
 }
